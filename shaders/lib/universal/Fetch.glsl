da0b2380f9b2870966c00c78d8fffe25290eefd0
HaringPro <132074996+HaringPro@users.noreply.github.com>
2025-09-11 21:46:26 +0800

    Update Fetch.glsl

--- shaders/lib/universal/Fetch.glsl
+++ shaders/lib/universal/Fetch.glsl
@@ -1,4 +1,3 @@
-
 float FetchDepthFix(in ivec2 texel) {
 	float depth = loadDepth0(texel);
 	return depth + 0.38 * step(depth, 0.56);
@@ -32,25 +31,3 @@ vec3 FetchWorldNormal(in ivec2 texel) {
 vec3 FetchWorldNormal(in uint data) {
 	return OctDecodeUnorm(Unpack2x8U(data));
 }
-
-#if defined DISTANT_HORIZONS
-float FetchLinearDepth(in ivec2 texel) {
-	float depth = loadDepth0(texel);
-	float linearDepth = ScreenToViewDepth(depth);
-	if (depth > 0.999999) linearDepth = ScreenToViewDepthDH(loadDepth0DH(texel));
-    return linearDepth;
-}
-float FetchLinearDepthSolid(in ivec2 texel) {
-	float depth = loadDepth1(texel);
-	float linearDepth = ScreenToViewDepth(depth);
-	if (depth > 0.999999) linearDepth = ScreenToViewDepthDH(loadDepth1DH(texel));
-    return linearDepth;
-}
-#else
-float FetchLinearDepth(in ivec2 texel) {
-    return ScreenToViewDepth(loadDepth0(texel));
-}
-float FetchLinearDepthSolid(in ivec2 texel) {
-    return ScreenToViewDepth(loadDepth1(texel));
-}
-#endif
