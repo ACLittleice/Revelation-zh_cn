8577a2aeb34bd6073a17a5a1ae73d1f8fcfb6593
HaringPro <132074996+HaringPro@users.noreply.github.com>
2025-10-03 20:24:41 +0800

    Update Render.glsl

--- shaders/lib/lighting/shadow/Render.glsl
+++ shaders/lib/lighting/shadow/Render.glsl
@@ -94,7 +94,7 @@ vec3 CalculateWaterCaustics(in vec3 worldPos, in float waterDepth, in float dith
 		caustics += saturate(1.0 - 512.0 * sdot(worldPos - refractedPos));
 	}
 
-	return -smin(-caustics, -0.2, 0.2) * exp2(-rLOG2 * waterExtinction * waterDepth);
+	return -smin(-caustics, -0.2, 0.2) * saturate(exp2(-rLOG2 * waterExtinction * waterDepth));
 }
 
 vec3 PercentageCloserFilter(in vec3 shadowScreenPos, in vec3 worldPos, in float dither, in float penumbraScale) {
@@ -135,7 +135,7 @@ vec3 PercentageCloserFilter(in vec3 shadowScreenPos, in vec3 worldPos, in float 
 
 			float waterDepth = waterData.x * shadowProjectionInverse[2].z * 5.0;
 			vec3 caustics = CalculateWaterCaustics(worldPos, waterDepth, dither);
-			result = mix(result, saturate(caustics), waterData.y * rSteps);
+			result = mix(result, caustics, waterData.y * rSteps);
 		}
 	#endif
 
