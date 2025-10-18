0b90090c832dd0e4ce4358a3d8a3ef912ab36c99
HaringPro <132074996+HaringPro@users.noreply.github.com>
2025-10-18 18:31:46 +0800

    Update Interpolate.glsl

--- shaders/lib/utility/Interpolate.glsl
+++ shaders/lib/utility/Interpolate.glsl
@@ -105,6 +105,51 @@ vec4 catmullRom(in float f) {
     );
 }
 
+// https://gist.github.com/TheRealMJP/bc503b0b87b643d3505d41eab8b332ae
+// The following code is licensed under the MIT license
+
+// Samples a texture with Catmull-Rom filtering, using 9 texture fetches instead of 16.
+// See http://vec3.ca/bicubic-filtering-in-fewer-taps/ for more details
+vec4 textureCatmullRom(in sampler2D tex, in vec2 uv) {
+	vec2 res = vec2(textureSize(tex, 0));
+    vec2 pixelSize = 1.0 / res;
+
+    vec2 samplePos = uv * res;
+    vec2 texPos1 = floor(samplePos - 0.5) + 0.5;
+
+    vec2 f = samplePos - texPos1;
+
+    vec2 w0 = f * (-0.5 + f * (1.0 - 0.5 * f));
+    vec2 w1 = 1.0 + f * f * (-2.5 + 1.5 * f);
+    vec2 w2 = f * (0.5 + f * (2.0 - 1.5 * f));
+    vec2 w3 = f * f * (-0.5 + 0.5 * f);
+
+    vec2 w12 = w1 + w2;
+    vec2 offset12 = w2 / (w1 + w2);
+
+    vec2 texPos0  = texPos1 - 1.0;
+    vec2 texPos3  = texPos1 + 2.0;
+    vec2 texPos12 = texPos1 + offset12;
+
+    texPos0  *= pixelSize;
+    texPos3  *= pixelSize;
+    texPos12 *= pixelSize;
+
+    vec4 result = vec4(0.0);
+    result += texture(tex, vec2(texPos0.x , texPos0.y)) * w0.x  * w0.y;
+    result += texture(tex, vec2(texPos12.x, texPos0.y)) * w12.x * w0.y;
+    result += texture(tex, vec2(texPos3.x , texPos0.y)) * w3.x  * w0.y;
+
+    result += texture(tex, vec2(texPos0.x , texPos12.y)) * w0.x  * w12.y;
+    result += texture(tex, vec2(texPos12.x, texPos12.y)) * w12.x * w12.y;
+    result += texture(tex, vec2(texPos3.x , texPos12.y)) * w3.x  * w12.y;
+
+    result += texture(tex, vec2(texPos0.x , texPos3.y)) * w0.x  * w3.y;
+    result += texture(tex, vec2(texPos12.x, texPos3.y)) * w12.x * w3.y;
+    result += texture(tex, vec2(texPos3.x , texPos3.y)) * w3.x  * w3.y;
+    return result;
+}
+
 // Approximation from SMAA presentation [Jimenez 2016]
 vec4 textureCatmullRomFast(in sampler2D tex, in vec2 coord) {
 	vec2 res = vec2(textureSize(tex, 0));
