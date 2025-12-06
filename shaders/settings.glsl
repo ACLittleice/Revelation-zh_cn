e3527cf668da1cc1bb56270939d517730b8138c6
HaringPro <132074996+HaringPro@users.noreply.github.com>
2025-12-06 22:05:19 +0800

    Update settings.glsl

--- shaders/settings.glsl
+++ shaders/settings.glsl
@@ -244,10 +244,6 @@ const ivec2 skyMapRes = ivec2(256, 256);
 	#define MOTION_BLUR_SAMPLES 6 // Sample count of motion blur. [2 3 4 5 6 7 8 9 10 12 14 16 18 20 22 24]
 	#define MOTION_BLUR_STRENGTH 0.5 // Strength of the motion blur. [0.0 0.05 0.1 0.15 0.2 0.25 0.3 0.35 0.4 0.45 0.5 0.6 0.7 0.8 0.9 1.0 1.2 1.4 1.5 1.7 2.0 2.5 3.0 3.5 4.0 4.5 5.0 7.0 10.0 12.0 14.0 16.0 18.0 20.0]
 
-	#if RENDER_MODE == 0
-		#undef MOTION_BLUR
-	#endif
-
 /* Bloom */
 	#define BLOOM_ENABLED // Enables bloom
 	#define BLOOMY_FOG // Enables bloomy fog
@@ -274,6 +270,11 @@ const ivec2 skyMapRes = ivec2(256, 256);
 /* FidelityFX */
 	#define CAS_ENABLED // Sharpens the final image using AMD FidelityFX CAS (Contrast-Adaptive Sharpening)
 	#define CAS_STRENGTH 0.3 // Strength of the CAS. [0.0 0.01 0.02 0.03 0.04 0.05 0.06 0.07 0.08 0.09 0.1 0.15 0.2 0.25 0.3 0.35 0.4 0.45 0.5 0.55 0.6 0.65 0.7 0.75 0.8 0.85 0.9 0.95 1.0]
+
+	#if RENDER_MODE == 0
+		#undef MOTION_BLUR
+		#undef CAS_ENABLED
+	#endif
 
 //======// Debug //===============================================================================//
 
