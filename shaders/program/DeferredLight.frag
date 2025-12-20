c0057fb55fa0a9ca295b7670cdcefad332d305fb
HaringPro <132074996+HaringPro@users.noreply.github.com>
2025-12-20 16:04:46 +0800

    GGX spherical area light approximation

--- shaders/program/DeferredLight.frag
+++ shaders/program/DeferredLight.frag
@@ -268,7 +268,7 @@ void main() {
 						const vec3 f0 = vec3(DEFAULT_DIELECTRIC_F0);
 					#endif
 
-					specularDirect = shadow * SpecularGGX(LdotH, NdotV, NdotL, NdotH, material.roughness, f0);
+					specularDirect = shadow * SphericalAreaGGX(LdotH, NdotV, NdotL, LdotV, material.roughness, f0);
 				}
 			}
 		}
--- shaders/settings.glsl
+++ shaders/settings.glsl
@@ -24,6 +24,8 @@ const float	shadowDistance 	  = 192.0; // [64.0 80.0 96.0 112.0 128.0 160.0 192.
 
 const ivec2 skyMapRes = ivec2(256, 256);
 
+#define SUN_RADIUS_MULT 2.0 // Multiplier of the sun radius (1.0 = real sun radius). [1.0 2.0 3.0 4.0 5.0 6.0 7.0 8.0 9.0 10.0 11.0 12.0 13.0 14.0 15.0 16.0 17.0 18.0 19.0 20.0 21.0 22.0 23.0 24.0 25.0 26.0 27.0 28.0 29.0 30.0 31.0 32.0 33.0 34.0 35.0 36.0 37.0 38.0 39.0 40.0]
+
 /* Clouds */
 	#define CLOUDS
 	#define CLOUD_SHADOWS
